f91184fb045e8220fc55a14f542f57b2bff9edca
Wilshawn <wilshawn416@verizon.net>
2017-02-28 21:42:59 -0500

    Add Php file for form submission (WIP)

--- php/acknowledge.php
+++ php/acknowledge.php
@@ -3,9 +3,15 @@ if(isset($_POST['submit'])) {
     $to = 'wilshawn.alexander@gmail.com'; // Email Address receipent
     $subject = 'World of Wilshawn New Contact Submission';
     $message = 'Name: ' . $_POST['name'] . "\r\n\r\n";
+    $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+    if ($email) {
+        $headers .= "\r\nReply-To: $email";
+    }
     $message .= 'Email: ' . $_POST['email'] . "\r\n\r\n";
     $message .= 'How can I help?: ' . $_POST['comments'];
-    echo $message;
+    $headers = "From: webmaster@wilshawn.com\r\n";
+    $headers .= 'Content-Type: text/plain; charset=utf-8';
+    $success = mail($to, $subject, $message, $headers);
 }
 ?>
 
@@ -15,9 +21,12 @@ if(isset($_POST['submit'])) {
         <title>WORLD OF WILSHAWN</title>
     </head>
     <body>
+        <?php if (isset($success) && $success) { ?>
         <h1>Thank You</h1>
-        <p>Your message has been sent.</p>
+        Your message has been sent.
+        <?php } else { ?>
         <h1>Oops!</h1>
-        <p>Sorry, there was a problem sending your message.</p>
+        Sorry, there was a problem sending your message.
+        <?php  ?>
     </body>
 </html>
